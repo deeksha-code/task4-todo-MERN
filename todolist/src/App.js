006be83e8b0f73f895c3a0991f691738f0335ed5
deeksha-code <deekshashetty736@gmail.com>
2023-11-01 23:18:23 +0530

    modified app,index

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,64 +1,22 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 import ItemCreate from "./components/ItemCreate";
 import ItemList from "./components/ItemList";
+import useItemsContext from "./hooks/use-items-context";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const { items, fetchItems } = useItemsContext();
 
-  const fetchItems = async () => {
-    const response = await axios.get("http://localhost:8000/getItems");
-    setItems(response.data);
- 
-  };
-  
   useEffect(() => {
     fetchItems();
-  }, []);
-
-  const editItemById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:8000/updateItem/${id}`, {
-      title: newTitle,
-    });
-    const updatedItems = items.map((item) => {
-      if (item.id === id) {
-        return { ...item, ...response.data };
-      }
-      return item;
-    });
-    setItems(updatedItems);
-  };
-
-  const deleteItemById = async (id) => {
-    await axios.delete(`http://localhost:8000/deleteItem/${id}`);
-
-    const updatedItems = items.filter((item) => {
-      return item.id !== id;
-    });
-
-    setItems(updatedItems);
-  };
-
-  const createItem = async (title) => {
-    const response = await axios.post("http://localhost:8000/createItem", {
-      title,
-    });
-    const updatedItems = [...items, response.data];
-
-    setItems(updatedItems);
-  };
+  }, [items]);
 
   return (
     <div>
       <h1>TO Do List APP</h1>
       <div className="container">
-        <ItemCreate onCreate={createItem} />
-        <ItemList
-          items={items}
-          onDelete={deleteItemById}
-          onEdit={editItemById}
-        />
+        <ItemCreate />
+        <ItemList />
       </div>
     </div>
   );
