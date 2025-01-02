dba728f2e9a3fd397539789309a734d847b08d8c
Deeksha Shetty <68739393+deeksha-code@users.noreply.github.com>
2025-01-02 10:54:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,79 +1,74 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const Item = require('./models/itemModel');
 
-mongoose.connect("mongodb+srv://deekshashetty736:liQEsHPRTvrM7Lrj@cluster0.ardch.mongodb.net/");
-
-const app=express();
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.post('/createItem',async (req,res)=>{
- 
+const MONGO_URI = "mongodb+srv://deekshashetty736:liQEsHPRTvrM7Lrj@cluster0.ardch.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+app.post('/createItem', async (req, res) => {
   try {
-   
-    const item=new Item({
-      itemName:req.body.title
-    })
-    const data=await item.save();
-    return res.send(data)
+    const item = new Item({
+      itemName: req.body.title,
+    });
+    const data = await item.save();
+    res.send(data);
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
   }
-})
+});
 
-app.get('/getItems',async (req,res)=>{
- 
-  try{
-    
-    const item=await Item.find({})
-    res.send(item)
-
-  }catch(err){
+app.get('/getItems', async (req, res) => {
+  try {
+    const items = await Item.find({});
+    res.send(items);
+  } catch (err) {
     console.log(err);
-    res.send(err)
-
+    res.status(500).send(err);
   }
-})
+});
 
-app.put('/updateItem/:id',async (req,res)=>{
- 
-  try{
-
-    const {id}=req.params;   
-    const updatedItem=await Item.findByIdAndUpdate({_id:id},{itemName:req.body.title},{new:true})
-
-    // const item=await Item.find({})
-    return res.send(updatedItem)
-  
-  }catch(err){
+app.put('/updateItem/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedItem = await Item.findByIdAndUpdate(
+      { _id: id },
+      { itemName: req.body.title },
+      { new: true }
+    );
+    res.send(updatedItem);
+  } catch (err) {
     console.log(err);
+    res.status(500).send(err);
   }
-})
+});
 
-app.delete('/deleteItem/:id',async (req,res)=>{
- 
-  try{
-
-    const {id}=req.params;
-    const deletedItem=await Item.findByIdAndDelete({_id:id})
-    return res.send(deletedItem);
-
-    // const item=await Item.find({})   
-    // return res.send(item)
-   
-
-  }catch(err){
-    return res.send(err)
-
+app.delete('/deleteItem/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedItem = await Item.findByIdAndDelete({ _id: id });
+    res.send(deletedItem);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
   }
-})
+});
 
-mongoose.connection.once("open", () => {
-    console.log("connected to mongoDB");
-    app.listen(8000, () => {
-      console.log("Server is listening on port 8000");
-    });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
